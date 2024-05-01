112e10eb74f76f240d26a961515b33ee5b6f0ba0
Michelle Mei-Ling Sandford <codesswa@outlook.com>
2024-05-01 00:31:26 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28633,7 +28633,10 @@ const Home = ({ name , title  })=>{
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
                 className: "background",
                 src: (0, _cwb1PngDefault.default),
-                alt: ""
+                alt: "",
+                style: {
+                    objectFit: "contain"
+                }
             }, void 0, false, {
                 fileName: "src/Components/Home.jsx",
                 lineNumber: 30,
